5d9e705e92d22cd789a229673b37b57aa696ee71
ehsanatwork <ehsanatwork@gmail.com>
2022-04-28 00:32:54 +0600

    add featured image

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,14 @@
 class Post < ApplicationRecord
+  # rich text
   has_rich_text :content
 
+  # validation
   validates :title, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 10, maximum: 60 }
   validates :content, presence: true, length: { minimum: 30 }
 
+  # relationship
   has_many :comments, dependent: :destroy
+
+  # Image
+  has_one_attached :featured_image
 end
