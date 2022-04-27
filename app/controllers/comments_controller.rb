64fb14c8f4031877e430910e90953a32a67c7e38
ehsanatwork <ehsanatwork@gmail.com>
2022-04-27 16:29:20 +0600

    add comment to post

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,45 @@
+class CommentsController < ApplicationController
+  before_action :set_post, only: %i[create edit update destroy]
+  before_action :set_comment, only: %i[edit update destroy]
+
+  def create
+    @comment = @post.comments.create(comment_params)
+    if @comment.save
+      flash[:success] = 'Thanks for your valuable comments.'
+      redirect_to @post
+    else
+      flash[:danger] = 'Something went wrong!'
+      redirect_to @post
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @comment.update(comment_params)
+      flash.now[:success] = 'Comment update successfully.'
+      redirect_to post_path(@post)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @comment.destroy
+    flash.now[:danger] = 'Comment deleted!'
+    redirect_to @post
+  end
+
+  private
+  def set_post
+    @post = Post.find(params[:post_id])
+  end
+
+  def set_comment
+    @comment = @post.comments.find(params[:id])
+  end
+
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
+end
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,6 @@ class Post < ApplicationRecord
 
   validates :title, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 10, maximum: 60 }
   validates :content, presence: true, length: { minimum: 30 }
+
+  has_many :comments, dependent: :destroy
 end
