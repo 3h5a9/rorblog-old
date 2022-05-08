8d844bd7c9805868dee305da35b547f192851855
ehsanatwork <ehsanatwork@gmail.com>
2022-05-09 03:20:01 +0600

    add user avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,16 @@
+# frozen_string_literal: true
+
+# User Model
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
+
   acts_as_voter
+
+  has_one_attached :avatar do |attachable|
+    attachable.variant :avataricon, resize_to_fill: [32, 32]
+    attachable.variant :avatarthumbnail, resize_to_fill: [300, 300]
+  end
 end
